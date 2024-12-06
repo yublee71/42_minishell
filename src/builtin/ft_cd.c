ce2ffc0de9843a82bcae6cb64c76336b23be35e1
tikocty <tikocty@gmail.com>
2024-12-06 19:23:03 +0000

    added env, exit; rewrite cd to update pwd & oldpwd

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -12,76 +12,125 @@
 
 #include "builtin.h"
 
-static char	*ft_strdup_from(const char *s, int start)
+static t_env *ft_get_env(char *name_look_for, t_env **env_arr) //retrieve the env by compare the name
 {
-	int		i;
-	int		count;
-	char	*arr;
+	t_env	**current;
+	int len;
 
-	i = 0;
-	count = 0;
-	if (!s[start]) // str is shorter than the index to start dup from e.g. *s = hello, start = 7
-		return (NULL);
-	while (s[count])
-		count++;
-	arr = malloc((count - start + 1) * sizeof(char));
-	if (arr == NULL)
-		return (NULL);
-	while (start < count)
+	current = env_arr;
+	len = ft_strlen(name_look_for);
+	while(*current)
 	{
-		arr[i] = s[start];
-		start++;
-		i++;
+		if(!ft_strncmp(name_look_for, (*current)->name, len))
+			return (*current);
+		current = &(*current)->next;
 	}
-	arr[i] = 0;
-	return (arr);
+	return (NULL);
 }
 
-static int	change_directory(char *path, char **env)
+static char *ft_get_parent_path(t_env **env_arr) //for cd .., return to the path above the current one, not oldpwd
 {
-	int		i;
-	char	*path_togo;
-	int		status;
+	t_env	**current;
+	t_env *current_pwd;
+	int i;
+	int	last_slash;
+	char	*parentpath;
 
 	i = 0;
-	if (!ft_strncmp(path, "OLDPWD=", 7) || !ft_strncmp(path, "HOME=", 5))
+	current = env_arr;
+	current_pwd = ft_get_env("PWD", current);
+	while(current_pwd->var[i])
 	{
-		while (env[i])
-		{
-			if (!ft_strncmp(env[i], path, ft_strlen(path)))
-			{
-				path_togo = ft_strdup_from(env[i], ft_strlen(path));
-				status = chdir(path_togo);
-				return (status);
-			}
-			i++;
-		}
+		if(current_pwd->var[i] == '/')
+			last_slash = i;
+		i++;
 	}
-	else
-		status = chdir(path);
-	return (status);
+	parentpath = ft_substr(current_pwd->var, 0, last_slash);
+	return(parentpath);
+}
+
+static int change_directory(char *path, t_env **env_arr)
+{
+    t_env *current_pwd;
+    t_env *old_pwd;
+    int status;
+
+    current_pwd = ft_get_env("PWD", env_arr);
+    old_pwd = ft_get_env("OLDPWD", env_arr);
+    status = chdir(path);
+    if (status)
+    {
+        printf("Error: cd path not found\n");
+        return status;
+    }
+    if (current_pwd && old_pwd)
+    {
+        free(old_pwd->var);
+        old_pwd->var = ft_strdup(current_pwd->var); 
+        free(current_pwd->var);
+        current_pwd->var = ft_strdup(path);
+    }
+    return status;
+}
+
+
+static char	*ft_join_env(char *argu, t_env **env_arr)
+{
+	t_env	**current;
+	t_env	*current_pwd;
+	char	*fullpath;
+    char    *path_slash;
+
+	current = env_arr;
+	current_pwd = ft_get_env("PWD", current);
+    path_slash = ft_strjoin(current_pwd->var, "/\0");
+	fullpath = ft_strjoin(path_slash, argu);
+    free(path_slash);
+	return(fullpath);
 }
 
 // cd  == go back HOME
-// cd .. == go to OLDPWD
+// cd .. == go to 1 step before
 // cd absolute path = go to that path
 // TODO: error control 
 // TODO: set the update the OLDPWD to PWD
 //                          PWD to path_to_go 
-int	ft_cd(char *path_to_go, char **env)
+int ft_cd(int argc, char **argv, t_env **env_arr)
 {
-	int	status;
+    int status;
+    char *path_togo;
+    t_env *home_env;
 
-	if (path_to_go == NULL)
-		status = change_directory("HOME=", env);
-	else if (!ft_strncmp(path_to_go, "..", 2))
-		status = change_directory("OLDPWD=", env);
-	else
-		status = change_directory(path_to_go, env);
-	if (status != 0)
-	{
-		printf("Error: cd\n");
-		exit(1);
-	}
-	return (0);
+    if (argc == 1) 
+    {
+        home_env = ft_get_env("HOME", env_arr);
+        if (!home_env || !home_env->var) 
+        {
+            printf("Error: HOME not set\n");
+            return -1;
+        }
+        path_togo = home_env->var;
+        status = change_directory(path_togo, env_arr);
+    } 
+    else if (argc == 2) 
+    {
+        if (!ft_strncmp(argv[1], "..", 2))
+            path_togo = ft_get_parent_path(env_arr);
+        else
+
+            path_togo = ft_join_env(argv[1], env_arr);
+        if (!path_togo) 
+        {
+            printf("Error: Unable to determine path\n");
+            return -1;
+        }
+        status = change_directory(path_togo, env_arr);
+        free(path_togo);
+    } 
+    else 
+    {
+        printf("Error: cd too many arguments\n");
+        return -1;
+    }
+    return (status);
 }
