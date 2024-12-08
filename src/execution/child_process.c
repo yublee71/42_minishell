9e0f63c55c1b85ac9c7e1caba92832840cfe041b
yublee71 <lybin71@naver.com>
2024-12-08 03:29:49 +0000

    fix: fix double free in case of invalid command

--- src/execution/child_process.c
+++ src/execution/child_process.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 00:34:13 by yublee            #+#    #+#             */
-/*   Updated: 2024/12/05 04:32:33 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/08 03:28:10 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,8 +82,8 @@ void	child_process(int i, t_ast *cmd_node, t_info *info)
 		exit_with_message(NULL, EXIT_SUCCESS, info);
 	if (is_builtin(args[0]) >= 0)
 		exit_with_message(args[0], call_builtin(cmd_node->args, info), info);
-	else
-		free_env(info->env_lst);
+	// else
+	// 	free_env(info->env_lst);
 	cmd_path = get_cmd_path(args[0], info->env);
 	if (access(cmd_path, X_OK) < 0)
 		exit_with_message(args[0], 127, info);
