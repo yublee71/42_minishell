4a80f71764ddf927b105ffa5bad97bcec80e6063
yublee71 <lybin71@naver.com>
2024-12-08 16:51:30 +0000

    fix: change the size of array of which_builtin

--- src/utils/env.c
+++ src/utils/env.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 00:17:01 by yublee            #+#    #+#             */
-/*   Updated: 2024/12/08 16:46:39 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/08 16:49:48 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	which_builtin(char *cmd)
 	i = 0;
 	if (!cmd)
 		return (-1);
-	while (i < 5)
+	while (i < 7)
 	{
 		if (ft_strlen(cmd) == ft_strlen(builtins[i])
 			&& !ft_strncmp(cmd, builtins[i], ft_strlen(builtins[i])))
