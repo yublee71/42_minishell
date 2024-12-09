93aefea9e077508d73d79cc93e9e2b5b09b759c4
yublee71 <lybin71@naver.com>
2024-12-09 03:07:29 +0000

    fix: handle env -i case

--- src/builtin/ft_env.c
+++ src/builtin/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 19:09:30 by tikochoi          #+#    #+#             */
-/*   Updated: 2024/12/08 03:36:55 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/09 02:52:52 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_env(t_info *info)
 	t_env	*current;
 
 	current = *(info->env_lst);
-	if (!current)
-		exit_with_message("env", EXIT_FAILURE, info);
 	while (current)
 	{
 		ft_printf("%s=%s\n", current->name, current->var);
