163c068c03846ba8597de347b2b17ea0afa07397
yublee71 <lybin71@naver.com>
2024-12-09 01:46:19 +0000

    chore: remove comment

--- src/builtin/main.c
+++ src/builtin/main.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:18:48 by yublee            #+#    #+#             */
-/*   Updated: 2024/12/09 00:11:00 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/09 01:31:57 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,4 @@ int	call_builtin(char **args, t_info *info) // combine to the set up function, s
 	else if (i == 6)
 		status = ft_unset(args[1], info->env_lst);
 	return (status);
-	//t_env *temp = *env_arr;
-	//while (temp)
-	//{
-	//	printf("%s=%s\n", temp->name, temp->var);
-	//	temp = temp->next;
-	//}
-	// ft_unset("PWD", env_arr);
-	//temp = *env_arr;
-	//while (temp) 
-	//{
-	//	printf("%s=%s\n", temp->name, temp->var);
-	//	temp = temp->next;
-	//}
-	// free(env_arr); // Free the array itself
 }
