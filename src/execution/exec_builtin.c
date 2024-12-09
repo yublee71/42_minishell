3e515cb658f9e3ee65a5c5965071632f7e0a9caa
yublee71 <lybin71@naver.com>
2024-12-09 02:49:56 +0000

    feat: handle input and output for builtins

--- src/execution/exec_builtin.c
+++ src/execution/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 01:29:40 by yublee            #+#    #+#             */
-/*   Updated: 2024/12/09 01:29:58 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/09 02:49:19 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 
 void	exec_builtin(t_ast *cmd_node, t_info *info)
 {
-	set_stdin(-1, cmd_node, info);
+	int	saved_stdin;
+	int	saved_stdout;
+
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	if (saved_stdin == -1 || saved_stdout == -1)
+		exit_with_message("dup", EXIT_FAILURE, info);
+	set_stdin(-1, cmd_node, info); // TODO: handle non-existing files
 	set_stdout(-1, cmd_node, info);
 	*(info->status) = call_builtin(cmd_node->args, info);
+	if (dup2(saved_stdin, STDIN_FILENO) == -1)
+		exit_with_message("dup2", EXIT_FAILURE, info);
+	if (dup2(saved_stdout, STDOUT_FILENO) == -1)
+		exit_with_message("dup2", EXIT_FAILURE, info);
 }
