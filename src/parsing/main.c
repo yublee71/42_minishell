b2c7b885cefbc42366bfe3569dda434433ac28e9
yublee71 <lybin71@naver.com>
2024-12-09 03:20:38 +0000

    chore: delete a comment

--- src/parsing/main.c
+++ src/parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 22:18:56 by yublee            #+#    #+#             */
-/*   Updated: 2024/12/09 00:44:54 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/09 03:15:30 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_ast	*parser(char *cmd, t_env **env_lst, int status)
 		write(2, "Syntax error\n", ft_strlen("Syntax error\n"));
 		return (NULL);
 	}
-	expand_env_var(token_list, env_lst, status); //TODO
+	expand_env_var(token_list, env_lst, status);
 	ft_lstiter(token_list, remove_quotes);
 	root = build_tree(token_list);
 	ft_lstclear(&token_list, free);
