2d524ecfd3898756151a09195a0cc27e0ca36979
Yubeen Lee <lybin71@naver.com>
2024-12-11 04:57:59 +0000

    fix(cd): get parent directory without memory leak, after chdir update pwd

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -14,43 +14,37 @@
 
 static char *ft_get_parent_path(char *str)
 {
-    int i = 0;
-    int last_slash = -1;
-    // char *parentpath;
+	int i = 0;
+	int last_slash = -1;
 
-    while (str[i])
-    {
-        if (str[i] == '/')
-            last_slash = i;
-        i++;
-    }
-    if (last_slash <= 0) // Root or no slash
-        return (ft_strdup("/"));
-    return (ft_substr(str, 0, last_slash));
+	while (str[i])
+	{
+		if (str[i] == '/')
+			last_slash = i;
+		i++;
+	}
+	if (last_slash <= 0)
+		return (ft_strdup("/"));
+	return (ft_substr(str, 0, last_slash));
 }
 
-// static void update_env(char *new_path, t_env **env_lst)
-// {
-//     t_env *current = *env_lst;
-//     t_env *temp_pwd = NULL;
-//     t_env *temp_oldpwd = NULL;
+static void update_pwd(t_env **env_lst)
+{
+	t_env	*current;
+	char	*temp;
 
-//     while (current)
-//     {
-//         if (ft_strncmp(current->name, "PWD") == 0)
-//             temp_pwd = current;
-//         else if (ft_strncmp(current->name, "OLDPWD") == 0)
-//             temp_oldpwd = current;
-//         current = current->next;
-//     }
-//     if (temp_pwd && temp_oldpwd)
-//     {
-//         free(temp_oldpwd->var);
-//         temp_oldpwd->var = ft_strdup(temp_pwd->var); 
-//         free(temp_pwd->var);
-//         temp_pwd->var = ft_strdup(new_path);
-//     }
-// }
+	current = *env_lst;
+	while (current)
+	{
+		if (ft_strlen(current->name) == 3
+			&& !ft_strncmp(current->name, "PWD", 3))
+			break;
+		current = current->next;
+	}
+	temp = current->var;
+	current->var = getcwd(NULL, 0);
+	free(temp);
+}
 
 static int	change_directory(char *path, t_env **env_lst)
 {
@@ -59,56 +53,54 @@ static int	change_directory(char *path, t_env **env_lst)
 	t_env	*current;
 
 	current = *env_lst;
+	status = 1;
 	while (current)
 	{
 		if (ft_strlen(current->name) == ft_strlen(path)
 			&& !ft_strncmp(current->name, path, ft_strlen(path)))
 		{
-			if (!ft_strncmp("PWD", path, ft_strlen(path))) //return to previous page
+			if (!ft_strncmp("PWD", path, ft_strlen(path)))
 				path_togo = ft_get_parent_path(current->var);
 			else
-				path_togo = current->var; //go bach "HOME"
+				path_togo = ft_strdup(current->var);
 			status = chdir(path_togo);
-			return (status);
+			update_pwd(env_lst);
+			free(path_togo);
+			break ;
 		}
 		current = current->next;
 	}
-	status = chdir(path);
 	return (status);
 }
 
-// cd  == go back HOME
-// cd .. == go to OLDPWD
-// cd absolute path = go to that path
-// TODO: error control 
-// TODO: set the update the OLDPWD to PWD
-//                          PWD to path_to_go 
+static int	change_directory_by_path(char *path, t_env **env_lst)
+{
+	int		status;
+
+	status = chdir(path);
+	update_pwd(env_lst);
+	return (status);
+}
+
 int	ft_cd(char **args, t_env **env_lst)
 {
 	int		status;
 	char	*path_to_go;
-	// t_env	*current;
+	char	*err_msg;
 
-	// printf("to go:%s\n", path_to_go);
-	// current = *env;
-	// while (current)
-	// {
-	// 	printf("name:%s\n",current->name);
-	// 	printf("val:%s\n",current->var);
-	// 	current = current->next;
-	// }
-	if (array_size((void **)args) != 2)
+	if (array_size((void **)args) > 2)
 	{
-		write(2, "cd: too many arguments\n", ft_strlen("cd: too many arguments\n"));
+		err_msg = "cd: too many arguments\n";
+		write(2, err_msg, ft_strlen(err_msg));
 		return (1);
 	}
 	path_to_go = args[1];
 	if (path_to_go == NULL)
 		status = change_directory("HOME", env_lst);
-	else if (!ft_strncmp(path_to_go, "..", 2))
-		status = change_directory("PWD", env_lst); //based on PWD, find the parents path
+	else if (ft_strlen(path_to_go) == 2 && !ft_strncmp(path_to_go, "..", 2))
+		status = change_directory("PWD", env_lst);
 	else
-		status = change_directory(path_to_go, env_lst);
+		status = change_directory_by_path(path_to_go, env_lst);
 	if (status != 0)
 	{
 		write(2, "cd: ", ft_strlen("cd :"));
